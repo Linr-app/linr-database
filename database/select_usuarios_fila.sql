789190661a929c87788253ffc57c2778cf739035
Denis Taveira <denis.taveira@hotmail.com>
2017-10-09 20:13:29 -0300

    Modificação Usuario Fila (#6)
    * Update sql HoraEntradaAtendimento e HoraSaidaAtendimento
    
    * Update e Select Atualizados
    
    * "As" maiúsculos
    
    * Espaço na última linha
    
    * Espaço na última linha 2
    
    * Linha a mais removida
    
    * Update User's Table in database
    
    * Add enter
    
    * Modificação Usuario Fila

--- database/select_usuarios_fila.sql
+++ database/select_usuarios_fila.sql
@@ -1,15 +1,15 @@
-SELECT *
-FROM Usuario
-LEFT JOIN UsuarioCadastrado
-ON Usuario.id_usuario=UsuarioCadastrado.id_usuario
-ORDER BY Usuario.id_usuario
-AS T1;
-
 SELECT UsuarioFila.id_usuario_fila, UsuarioFila.id_fila, UsuarioFila.hora_entrada_fila,
 UsuarioFila.qtd_pessoas, UsuarioFila.tem_reserva, T1.id_usuario, T1.nome, T1.telefone, T1.email
-FROM UsuarioFila
-INNER JOIN T1
+  FROM UsuarioFila
+INNER JOIN (
+	SELECT *
+	FROM Usuario
+	LEFT JOIN UsuarioCadastrado
+	ON Usuario.id_usuario=UsuarioCadastrado.id_usuario
+	ORDER BY Usuario.id_usuario
+) AS T1 
 ON UsuarioFila.id_usuario=T1.id_usuario
 ORDER BY UsuarioFila.id_usuario_fila
-WHERE id_fila  =  $1 AND UsuarioFila.hora_entrada_atendimento = NULL
-AS T2;
+   WHERE id_fila  = $1
+     AND UsuarioFila.hora_entrada_atendimento = NULL
+      AS T2;
